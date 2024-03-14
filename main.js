a72d988f4ae1b25d13e9f9787d213f86817b8508
baxtiyorov2013 <baxtiyorov2013>
2024-03-14 11:15:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("Sizda hozir random sonlar chiqishni boshledi")
+
+let son = Math.floor(Math.random() * 100)
+
+alert("Random orqali chiqqan son: " + (son));
+
+alert("biz random sonni 2 ga kopaytirmoqchi va bo'lmoqchimiz !")
+
+alert("Random orqali chiqqan son: " + (son) + " edi" + "\n" + "biz uni 2 ga ko'paytirib " + (son * 2) + " sonini chiqardik," + "\n" + "va shu sonni 2 ga bo'lib: " + (son / 2) + " sonni chiqardik," + "\n" + "va shu sonni 3 ga bolgandagi qoldiq: " + (son % 3) + " sonini chiqardik,")
